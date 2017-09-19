d724d31ba44144f6ea5e6cd5f4ab28629b646efd
Spaituk <Spaituk_7@hotmail.com>
2017-09-19 14:51:24 +0200

    Separation de la requete dans un fichier modele.php

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php 
 // Accès aux données
-$bdd = new PDO('mysql:host=localhost;dbname=mvc;charset=utf8', 'ts2', 'ts2');
-$billets = $bdd->query('select BIL_ID as id, BIL_DATE as date,'
-  . ' BIL_TITRE as titre, BIL_CONTENU as contenu from T_BILLET'
-  . ' order by BIL_ID desc');
+require 'Modele.php';
 
+$billets = getBillets();
 // Affichage
 require 'vueAccueil.php';
