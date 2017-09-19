506a2832a08a55625085bcd28ccac524e2b2bb75
Spaituk <Spaituk_7@hotmail.com>
2017-09-19 14:27:20 +0200

    Separation du html et du php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8" />
         <link rel="stylesheet" href
-        +="style.css" />
+        ="style.css" />
         <title>Mon Blog</title>
     </head>
     <body>
